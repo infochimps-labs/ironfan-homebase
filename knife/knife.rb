ced0fc296dda142c667aeaae630316af006240a0
Philip (flip) Kromer <flip@infochimps.org>
2012-02-17 17:41:21 -0600

    knife.rb now works even if the .chef dir is a symlink (eg ~/.chef)

--- knife/knife.rb
+++ knife/knife.rb
@@ -4,13 +4,11 @@
 # homebase     - default location for clusters, cookbooks and so forth
 #
 username            ENV['CHEF_USER'] || ENV['USER']
-homebase            ENV['CHEF_HOMEBASE'] ? File.expand_path(ENV['CHEF_HOMEBASE']) : File.expand_path("..", File.dirname(__FILE__))
+homebase            ENV['CHEF_HOMEBASE'] ? File.expand_path(ENV['CHEF_HOMEBASE']) : File.expand_path("..", File.realdirpath(File.dirname(__FILE__)))
 
 #
 # Additional settings and overrides
 #
-
-def load_if_exists(file) ; load(file) if File.exists?(file) ; end
 
 #
 # Clusters, cookbooks and roles
@@ -23,7 +21,7 @@ role_path           [ "#{homebase}/roles"     ]
 # Keys and cloud-specific settings.
 # Be sure all your .pem files are non-readable (mode 0600)
 #
-credentials_path    File.expand_path("credentials", File.dirname(__FILE__))
+credentials_path    File.expand_path("credentials", File.realdirpath(File.dirname(__FILE__)))
 client_key_dir      "#{credentials_path}/client_keys"
 ec2_key_dir         "#{credentials_path}/ec2_keys"
 
@@ -47,6 +45,8 @@ cache_options           :path => "/tmp/chef-checksums-#{username}"
 bootstrap_runs_chef_client true
 bootstrap_chef_version  "~> 0.10.4"
 
+def load_if_exists(file) ; load(file) if File.exists?(file) ; end
+
 # Organization-sepecific settings -- Chef::Config[:ec2_image_info] and so forth
 #
 # This must do at least these things:
@@ -56,5 +56,6 @@ bootstrap_chef_version  "~> 0.10.4"
 #
 #
 load_if_exists "#{credentials_path}/knife-org.rb"
+
 # User-specific knife info or credentials
 load_if_exists "#{credentials_path}/knife-user-#{username}.rb"
