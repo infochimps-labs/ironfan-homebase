5eee7480e62a6bbb679ef295a6ff9ffd3b49f0ec
Philip (flip) Kromer <flip@infochimps.org>
2012-07-02 13:56:17 -0500

    look for *all* NFS servers in the control cluster, not just enumerated ones

--- environments/_default.rb
+++ environments/_default.rb
@@ -3,7 +3,7 @@ require 'chef/mixin/deep_merge'
 # Default attributes to include in every environment
 $_default_environment = {
   :discovers => {
-    :nfs        => { :server => 'control' },
+    :nfs        => 'control',
     :zabbix     => { :server => 'control' },
   },
   :route53 => { :zone => "YOURDOMAIN.com" },
