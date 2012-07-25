4358011fe53e64d644035943e7cb2d9470c21f35
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-07-25 13:20:58 -0500

    Symlinked iron_cuke role from vender/ironfan-pantry

--- roles/iron_cuke.rb
+++ roles/iron_cuke.rb
@@ -1,7 +1,1 @@
-name        'iron_cuke'
-description 'Validates a machine against its announced contract'
-
-run_list *%w[
-  iron_cuke
-  iron_cuke::judge
-  ]
+../vendor/ironfan-pantry/roles/iron_cuke.rb
