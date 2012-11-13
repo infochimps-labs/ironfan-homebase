ad84560aee63e7a8cba6438df0c3c299106337f0
Nathaniel Eliot <temujin9@t9productions.com>
2012-11-13 12:20:47 -0600

    Removing enterprise cookbooks from open-source homebase

--- config/Berksfile.inc.rb
+++ config/Berksfile.inc.rb
@@ -2,7 +2,6 @@ USE_LOCAL       = false                                 if USE_LOCAL.nil?
 LOCAL_PATH      = "vendor"                              if LOCAL_PATH.nil?
 OPSCODE_REPO    = 'infochimps-labs/opscode_cookbooks'   if OPSCODE_REPO.nil?
 PANTRY_REPO     = 'infochimps-labs/ironfan-pantry'      if PANTRY_REPO.nil?
-ENTERPRISE_REPO = 'infochimps/ironfan-enterprise'       if ENTERPRISE_REPO.nil?
 
 def github_cookbook(name, repo, rel)
   if USE_LOCAL
@@ -19,10 +18,6 @@ end
 
 def pantry_cookbook(name)
   github_cookbook name, PANTRY_REPO, ('cookbooks/' + name)
-end
-
-def enterprise_cookbook(name)
-  github_cookbook name, ENTERPRISE_REPO, ('cookbooks/' + name)
 end
 
 def org_cookbook(name)
