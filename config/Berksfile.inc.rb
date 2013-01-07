72b2aeb353cc668b80a7b5c5e378ca5b3b8ad4e4
Nathaniel Eliot <temujin9@t9productions.com>
2013-01-07 14:38:48 -0600

    Berksfile.inc: cleanup PANTRY_BRANCH config

--- config/Berksfile.inc.rb
+++ config/Berksfile.inc.rb
@@ -1,19 +1,19 @@
-USE_LOCAL       = false                                 unless defined? USE_LOCAL
-LOCAL_PATH      = "vendor"                              unless defined? LOCAL_PATH
-PANTRY_REPO     = 'infochimps-labs/ironfan-pantry'      unless defined? PANTRY_REPO
-PANTRY_BRANCH   = 'master'                              unless defined? PANTRY_BRANCH
+USE_LOCAL         = false                               unless defined? USE_LOCAL
+LOCAL_PATH        = "vendor"                            unless defined? LOCAL_PATH
+PANTRY_REPO       = 'infochimps-labs/ironfan-pantry'    unless defined? PANTRY_REPO
+PANTRY_BRANCH     = 'master'                            unless defined? PANTRY_BRANCH
 
-def github_cookbook(name, repo, rel)
+def github_cookbook(name, repo, rel, branch)
   if USE_LOCAL
     r_name = repo.split('/')[1]
     cookbook name, path: "#{LOCAL_PATH}/#{r_name}/#{rel}"
   else
-    cookbook name, github: repo, protocol: 'ssh', rel: rel, branch: PANTRY_BRANCH
+    cookbook name, github: repo, protocol: 'ssh', rel: rel, branch: branch
   end
 end
 
 def pantry_cookbook(name)
-  github_cookbook name, PANTRY_REPO, ('cookbooks/' + name)
+  github_cookbook name, PANTRY_REPO, ('cookbooks/' + name), PANTRY_BRANCH
 end
 
 def org_cookbook(name)
