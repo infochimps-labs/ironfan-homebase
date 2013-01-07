a5c6b3d0b50328af6d1ff4db605e0db9f39d7163
Nathaniel Eliot <temujin9@t9productions.com>
2013-01-07 14:33:17 -0600

    Berksfile.inc: removing opscode_cookbook, adding PANTRY_BRANCH config

--- config/Berksfile.inc.rb
+++ config/Berksfile.inc.rb
@@ -1,19 +1,15 @@
 USE_LOCAL       = false                                 unless defined? USE_LOCAL
 LOCAL_PATH      = "vendor"                              unless defined? LOCAL_PATH
-OPSCODE_REPO    = 'infochimps-labs/opscode_cookbooks'   unless defined? OPSCODE_REPO
 PANTRY_REPO     = 'infochimps-labs/ironfan-pantry'      unless defined? PANTRY_REPO
+PANTRY_BRANCH   = 'master'                              unless defined? PANTRY_BRANCH
 
 def github_cookbook(name, repo, rel)
   if USE_LOCAL
     r_name = repo.split('/')[1]
     cookbook name, path: "#{LOCAL_PATH}/#{r_name}/#{rel}"
   else
-    cookbook name, github: repo, protocol: 'ssh', rel: rel
+    cookbook name, github: repo, protocol: 'ssh', rel: rel, branch: PANTRY_BRANCH
   end
-end
-
-def opscode_cookbook(name)
-  github_cookbook name, OPSCODE_REPO, name
 end
 
 def pantry_cookbook(name)
