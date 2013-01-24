d9341b933cea67290218cfc1aee259c74b4d8138
Josh Bronson <josh.bronson@gmail.com>
2013-01-24 14:44:52 -0600

    grabbing hue password from Chef::Config and running hue_config and hue_mysql_setup from hadoop_hue role

--- roles/hadoop_hue.rb
+++ roles/hadoop_hue.rb
@@ -3,10 +3,15 @@ description 'Installs hadoop_hue.'
 
 run_list %w[
   hadoop_cluster::hue
+  hadoop_cluster::hue_config
+  hadoop_cluster::hue_mysql_setup
 ]
 
 override_attributes({
                       :hadoop => {
+                        :hue => {
+                          :mysql_user_password => Chef::Config[:hive_mysql_user_password]
+                        },
                         :jobtracker => {
                           :plugins => ['org.apache.hadoop.thriftfs.ThriftJobTrackerPlugin'],
                         },
