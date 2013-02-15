1dd2d706e0ff02382ab81cd246bae8bb7b2f6621
Nathaniel Eliot <temujin9@t9productions.com>
2013-02-15 17:51:32 -0600

    org_base: migrating defaults from environments/_default.rb

--- roles/org_base.rb
+++ roles/org_base.rb
@@ -2,7 +2,12 @@ name 'org_base'
 description 'Attributes and recipes applied to EVERY node in the organization'
 
 default_attributes({
-  })
+  :discovers => {
+    :nfs        => 'control',
+    :zabbix     => { :server => 'control' },
+  },
+  :route53 => { :zone => "YOURDOMAIN.com" },
+})
 
 run_list(*%w[
 ])
