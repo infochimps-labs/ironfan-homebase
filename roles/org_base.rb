cbc964e4b67de1f6b2ccd8b917ad5d44f5b75110
Nathaniel Eliot <temujin9@t9productions.com>
2013-03-27 13:02:04 -0500

    org_base: adding prescription for replacing :discovers hash

--- roles/org_base.rb
+++ roles/org_base.rb
@@ -2,7 +2,9 @@ name 'org_base'
 description 'Attributes and recipes applied to EVERY node in the organization'
 
 default_attributes({
-  :discovers => {
+  # :nfs                  => { :discover_in => 'control' } ,
+  # :zabbix =>  { :server => { :discover_in => 'control' } },
+  :discovers => {  # FIXME: Replace with the above, once silverware 3.3 is everywhere
     :nfs        => 'control',
     :zabbix     => { :server => 'control' },
   },
