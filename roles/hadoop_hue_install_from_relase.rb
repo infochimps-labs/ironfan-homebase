7a36464b0b48c49d2371d6235b7dae792c5058b7
Josh Bronson <josh.bronson@gmail.com>
2013-08-23 17:30:51 -0500

    adding hue install from release role

---
+++ roles/hadoop_hue_install_from_relase.rb
@@ -0,0 +1,28 @@
+name 'hadoop_hue'
+description 'Installs hadoop_hue.'
+
+run_list %w[
+  hadoop_cluster::hue_install_from_release
+  hadoop_cluster::hue_config
+  hadoop_cluster::hue_mysql_setup
+]
+
+override_attributes({
+                      :hadoop => {
+                        :hue => {
+                          :mysql_user_password => Chef::Config[:hive_mysql_user_password]
+                        },
+                        :jobtracker => {
+                          :plugins => ['org.apache.hadoop.thriftfs.ThriftJobTrackerPlugin'],
+                        },
+                        :namenode => {
+                          :plugins => ['org.apache.hadoop.thriftfs.NamenodePlugin'],
+                        },
+                        :datanode => {
+                          :plugins => ['org.apache.hadoop.thriftfs.DatanodePlugin'],
+                        },
+                        :thrift => {
+                          :port => 10090,
+                        },
+                      }
+                    })
