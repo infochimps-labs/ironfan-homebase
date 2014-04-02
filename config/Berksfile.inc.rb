cfbd1f35f158b73b70ee93d932555af204430669
Josh Bronson <josh.bronson@gmail.com>
2014-04-01 21:03:45 -0500

    feature: pin to release_6
    Change-Id: I3e916c6a1ca304bbc17cde9c593ace13826b65f9

--- config/Berksfile.inc.rb
+++ config/Berksfile.inc.rb
@@ -16,7 +16,7 @@ end
 # ENV['USE_LOCAL']       = nil                                 # This is already default
 ENV['LOCAL_PATH']        = "vendor"                            unless ENV['LOCAL_PATH']
 ENV['PANTRY_REPO']       = 'infochimps-labs/ironfan-pantry'    unless ENV['PANTRY_REPO']
-ENV['PANTRY_BRANCH']     = 'master'                            unless ENV['PANTRY_BRANCH']
+ENV['PANTRY_BRANCH']     = 'release_6'                         unless ENV['PANTRY_BRANCH']
 ENV['COMMUNITY_REPO']    = 'infochimps-labs/community-pantry'  unless ENV['COMMUNITY_REPO']
 ENV['COMMUNITY_BRANCH']  = 'master'                            unless ENV['COMMUNITY_BRANCH']
 
